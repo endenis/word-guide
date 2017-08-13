92a1c390e54e85bd4c1e7b2604ed2482394242c9
endenis <endenis@gmail.com>
2017-08-13 18:00:43 +0200

    Added support of file with known words

--- word_stats.rb
+++ word_stats.rb
@@ -1,8 +1,18 @@
-file_name = ARGV.first
+file_name = ARGV[0]
+corpus_file_name = ARGV[1]
 raise 'No input filename' unless file_name
 
 POS_BLACKLIST = %w( DET P+D PUNC NUM )
 LEMMA_BLACKLIST = %w( d )
+
+known_words = []
+
+if corpus_file_name
+  File.open(corpus_file_name).each do |raw_line|
+    word = raw_line.chomp
+    known_words << word
+  end
+end
 
 vocabulary = {}
 
@@ -10,12 +20,12 @@ File.open(file_name).each do |raw_line|
   line = raw_line.chomp
   word, pos, raw_lemma = line.split "\t"
   lemma = raw_lemma.downcase
-  unless POS_BLACKLIST.include?(pos) || LEMMA_BLACKLIST.include?(lemma)
+  unless POS_BLACKLIST.include?(pos) || LEMMA_BLACKLIST.include?(lemma) || known_words.include?(lemma)
     vocabulary[lemma] ||= 0
     vocabulary[lemma] += 1 
   end
 end
 
 vocabulary.sort_by(&:last).reverse.to_h.each do |lemma, count|
-  puts "#{ lemma }\t#{ count }"
+  puts "#{ lemma }"#\t#{ count }"
 end
